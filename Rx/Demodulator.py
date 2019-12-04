e82df22a35f23cf191ab9e85ea95688e2f393fd8
Freesail <imperialfreesail@gmail.com>
2019-12-04 16:53:39 +0000

    data

--- Rx/Demodulator.py
+++ Rx/Demodulator.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 
 
 class Demodulator:
-    def __init__(self, sample_freq=1e3):
+    def __init__(self, sample_freq=1e3, channel_id='single', range=2000):
         self.src_queue = queue.Queue(maxsize=0)
         self.fs = sample_freq
+        self.channel_id = channel_id
+        self.range = range
 
         try:
             self.pyro_params = np.genfromtxt('./result/cal/pyro_params.csv', delimiter=',')
         except OSError:
             self.pyro_params = None
 
-        self.wl_range = (1, 20)
+        self.channel_trans, self.wavelength = self.get_channel_info()
+
         self.Te = 293.15
         self.thread = threading.Thread(target=self.demodulate)
 
@@ -92,6 +95,42 @@ class Demodulator:
                 y.append(T)
         return np.array(y)
 
+    def get_channel_info(self):
+        bpf = np.genfromtxt('./data/filter/%s.csv' % self.channel_id, delimiter=',', dtype=np.float64)
+        bpf_trans = interpolate.interp1d(bpf[:, 0], bpf[:, 1], bounds_error=True)
+
+        atmos = np.genfromtxt('./data/atoms/%s.csv' % self.range)
+        atmos_trans = interpolate.interp1d(atmos[:, 0], atmos[:, 1], bounds_error=True)
+
+        max_wl, min_wl = bpf[:, 0].max(), bpf[:, 0].min()
+        wavelength = np.concatenate((bpf[:, 0], atmos[:, 0]))
+        wavelength = np.sort(wavelength[(wavelength <= max_wl) & (wavelength >= min_wl)])
+
+        def channel_trans(wl):
+            # 0.72 for ZnSe lense
+            return 0.72 * 0.72 * atmos_trans(wl) * bpf_trans(wl) * bpf_trans(wl)
+
+        return channel_trans, wavelength
+        # return lambda x: 1.0, np.arange(0.1, 50, 0.1)
+
+    def planck_equ(self, wl, T):
+        c1 = 1.1910428661813628e8
+        c2 = 1.438776749195487e4
+        # W/Sr/m^2/um
+        return c1 / (wl ** 5) / (np.exp(c2 / wl / T) - 1)
+
+    def get_power(self, T, integrate_method='trapz'):
+        def rx_spectrum(wl):
+            return self.planck_equ(wl, T) * self.channel_trans(wl)
+
+        if integrate_method == 'trapz':
+            x = self.wavelength
+            y = rx_spectrum(self.wavelength)
+            return np.trapz(y, x=x) * np.pi * 0.8 * 2.89e-6
+        elif integrate_method == 'quad':
+            result = integrate.quad(rx_spectrum, self.wavelength[0], self.wavelength[-1])
+            return result[0] * np.pi * 0.8 * 2.89e-6
+
     @staticmethod
     def pyro_v_step(x, p0, p1, p2, p3):
         return p0 * (np.exp(-p1 * x) - np.exp(-p2 * x)) + p3
@@ -102,37 +141,26 @@ class Demodulator:
         dx1dt = - (self.pyro_params[0] * x[1] + self.pyro_params[1] * x[0] + self.pyro_params[2] * dpdt(t))
         return [dx0dt, dx1dt]
 
-    def planck_equ_real(self, wl, T):
-        c1 = 1.1910428661813628e-16  # 2hc^2
-        c2 = 0.01438776749195487  # hc/{k_b}
-        return c1 / (wl ** 5) / (np.exp(c2 / wl / T) - 1)
-
-    # values proportional to planck equ for simplicity
-    def planck_equ_prop(self, wl, T):
-        # wl in um, T in K
-        pass
-
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     d = Demodulator()
-    # wl = np.arange(1, 20, 0.1) * 1e-6
-    # p = d.planck_equ_real(wl, 1023)
+    print(d.get_power(1023, integrate_method='trapz'))
+    # wl = np.arange(1, 20, 0.1)
+    # p = d.planck_equ(wl, 1023)
     # p = p * np.pi * 0.8 * 2.89e-6
     #
-    #
-    #
-    # plt.plot(wl * 1e6, p)
+    # plt.plot(wl, p)
     # plt.show()
 
-    t = np.arange(0, 50e-3, 1e-3)
-    y0 = 293.15
-    w = 0.8978
-
-    y = d.torch_ode_piecewise_series_solv(t, y0, w, order=3)
-    # print(y)
-    plt.plot(t, y)
-    plt.xlim([0, 50e-3])
-    plt.ylim([200, 1100])
-    plt.show()
+    # t = np.arange(0, 50e-3, 1e-3)
+    # y0 = 293.15
+    # w = 0.8978
+    #
+    # y = d.torch_ode_piecewise_series_solv(t, y0, w, order=3)
+    # # print(y)
+    # plt.plot(t, y)
+    # plt.xlim([0, 50e-3])
+    # plt.ylim([200, 1100])
+    # plt.show()
